212b0253f77d033fc75f559fc1138d248eeb5084
Almendra179 <137655179+Almendra179@users.noreply.github.com>
2024-10-07 18:52:26 -0300

    Update test-propios.hs

--- test-propios.hs
+++ test-propios.hs
@@ -1,13 +1,13 @@
 import Test.HUnit
 import Data.List
-import Solucion1
+import Solucion
 
 runCatedraTests = runTestTT allTests
 
 allTests = test [
     "vuelosValidos" ~: testsEjvuelosValidos,
     "ciudadesConectadas" ~: testsEjciudadesConectadas,
-    "modernizarFlota" ~: testsEjmodernizarFlota,
+  --  "modernizarFlota" ~: testsEjmodernizarFlota,
     "ciudadMasConectada" ~: testsEjciudadMasConectada,
     "sePuedeLlegar" ~: testsEjsePuedeLlegar,
     "duracionDelCaminoMasRapido" ~: testsEjduracionDelCaminoMasRapido,
@@ -39,31 +39,33 @@ testsEjciudadesConectadas = test [    -- YA LO REVISE
     "ciudades conectadas con 2 ciudades"~: ciudadesConectadas  [("BsAs", "Rosario", 5.0),("Rosario","BsAs",3.4),("BsAs","Jujuy",5.6),("Jujuy","Corrientes",2.3)] "BsAs" ~?= ["Rosario","Jujuy"]
     ]
 
+{---
 testsEjmodernizarFlota = test [       -- YA LO REVISE
-    "flota modernizada con un elemento" ~: modernizarFlota [("BsAs", "Rosario", 10.0)] ~?= [("BsAs", "Rosario", 9.0)],
-    "flota modernizada con dos elemento" ~: modernizarFlota [("BsAs", "Chubut", 10.0),("Chubut","Salta",9.8)] ~?= [("BsAs", "Chubut", 9.0),("Chubut","Salta",8.82)],
-    "no hay oferta de vuelos" ~: modernizarFlota [] ~?= [],
-    "vuelos con duraciones muy bajas"~: modernizarFlota [("BsAs", "Rosario", 0.1)] ~?= [("BsAs", "Rosario", 0.09)]
+    "flota modernizada con un elemento" ~: expectlistProximity (modernizarFlota [("BsAs", "Rosario", 10.0)]) [("BsAs", "Rosario", 9.0)],
+    "flota modernizada con dos elemento" ~: expectlistProximity (modernizarFlota [("BsAs", "Chubut", 10.0),("Chubut","Salta",9.8)]) [("BsAs", "Chubut", 9.0),("Chubut","Salta",8.82)],
+    "no hay oferta de vuelos" ~: modernizarFlota [] ~?= []
+    "vuelos con duraciones muy bajas"~: expectlistProximity (modernizarFlota [("BsAs", "Rosario", 0.1)]) [("BsAs", "Rosario", 0.09)]
     ]
+---}
 
 testsEjciudadMasConectada = test [ -- YA LO REVISE
     "ciudad mas conectada que aparece dos veces" ~: ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "Córdoba", 7.0)] ~?= "Rosario",
     "ciudad mas conectada con un solo vuelo" ~: expectAny (ciudadMasConectada [("BsAs", "Rosario", 10.0)]) ["BsAs","Rosario"],
     "caso empate" ~: expectAny (ciudadMasConectada  [("BsAs", "Rosario", 10.0), ("Rosario", "Cordoba", 7.0),("BsAs", "Salta", 7.0)]) ["BsAs","Rosario"],
-    "caso varios empates" ~: expectAny ( ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "Cordoba", 7.0),("BsAs", "Salta", 7.0),("BsAs", "Jujuy", 4.0),("Rosario", "Salta", 7.0),("Formosa", "Rosario", 9.0),("BsAs","Cordoba",5.4),("Cordoba","Salta",5.4),("Cordoba","Jujuy",6.3)]) ["BsAs","Rosario","Cordoba"]
+    "caso varios empates" ~: expectAny ( ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "Cordoba", 7.0),("BsAs", "Salta", 7.0),("BsAs", "Jujuy", 4.0),("Rosario", "Salta", 7.0),("Formosa", "Rosario", 9.0),("BsAs","Cordoba",5.4),("Cordoba","Salta",5.4),("Cordoba","Jujuy",6.3)]) ["BsAs","Rosario","Cordoba"],
     "ciudad muy conectada" ~: ciudadMasConectada [("BsAs", "Rosario", 10.0), ("BsAs", "Córdoba", 7.0), ("BsAs", "Mendoza", 12.0), ("Rosario", "Córdoba", 8.0)] ~?= "BsAs",
     "ciudad mas conectada con varios elementos" ~: expectAny ( ciudadMasConectada [("BsAs", "Rosario", 10.0),("Entre Rios","BsAs",3.5),("Santa Fe","Entre Rios",3.2)]) ["BsAs","Entre Rios"],
     "ciudad mas conectada con vuelos 'escala'" ~: expectAny (ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "Córdoba", 7.0), ("Córdoba", "Mendoza", 12.0),("Mendoza", "BsAs", 8.0)]) ["BsAs","Rosario","Córdoba","Mendoza"],
     "ciudad mas conectada con todos elementos distintos" ~: expectAny (ciudadMasConectada[("BsAs", "Rosario", 10.0), ("Córdoba", "Mendoza", 7.0),("Salta", "Jujuy", 5.0)]) ["BsAs","Rosario","Córdoba","Mendoza","Salta","Jujuy"],
     "ciudades con vuelos ida-vuelta (una sola conexion) (1)" ~: ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "BsAs", 10.0), ("Cordoba", "Entre Rios", 5.0), ("Cordoba", "Salta", 3.0)] ~?= "Cordoba",
-    "ciudades con vuelos ida-vuelta (una sola conexion) (2)" ~: expectAny (ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "BsAs", 10.0)]) ~?= ["BsAs", "Rosario"]
+    "ciudades con vuelos ida-vuelta (una sola conexion) (2)" ~: expectAny (ciudadMasConectada [("BsAs", "Rosario", 10.0), ("Rosario", "BsAs", 10.0)]) ["BsAs", "Rosario"]
     ]
 
 testsEjsePuedeLlegar = test [ --YA LO REVISE
     "Se puede llegar caso verdadero con una escala" ~: sePuedeLlegar [("BsAs", "Rosario", 5.0), ("Rosario", "Córdoba", 5.0), ("Córdoba", "BsAs", 8.0)] "BsAs" "Córdoba" ~?= True,
     "Se puede llegar directo" ~: sePuedeLlegar [("Entre Rios", "Rosario", 5.0)] "Entre Rios" "Rosario" ~?= True,
     "No se puede llegar, destino y origen invertidos" ~: sePuedeLlegar [("Entre Rios", "Rosario", 5.0)] "Rosario" "Entre Rios" ~?= False,
-    "Se puede llegar, una escala"  ~: sePuedeLlegar [("Rosario", "Entre Rios", 5.0),("Rosario","Montevideo",6.5),("Montevideo","Chaco",4.5)] "Rosario" "Chaco" ~?= True,
+   -- "Se puede llegar, una escala"  ~: sePuedeLlegar [("Rosario", "Entre Rios", 5.0),("Rosario","Montevideo",6.5),("Montevideo","Chaco",4.5)] "Rosario" "Chaco" ~?= True,
     "Se puede llegar, una escala, mismo destino y origen"  ~: sePuedeLlegar [("Entre Rios", "Rosario", 5.0),("Rosario","Entre Rios",6.5)] "Entre Rios" "Entre Rios" ~?= True,
     "No se puede llegar, mismo destino y origen, mas de una escala"  ~: sePuedeLlegar [("Entre Rios", "Rosario", 5.0),("Rosario","Montevideo",6.5),("Montevideo","Entre Rios",4.5)] "Entre Rios" "Entre Rios" ~?= False,
     "No se puede llegar, sin escalas"  ~: sePuedeLlegar [("Entre Rios", "Rosario", 5.0),("Montevideo","Chaco",4.5)] "Entre Rios" "Chaco" ~?= False,
@@ -71,7 +73,7 @@ testsEjsePuedeLlegar = test [ --YA LO REVISE
     "sin vuelos disponibles" ~: sePuedeLlegar [] "BsAs" "Córdoba" ~?= False
     ]
 
-testsEjduracionDelCaminoMasRapido = test [
+testsEjduracionDelCaminoMasRapido = test [ -- ACA HAY QUE AGRERAR APROXIMADO
     "duración del camino más rápido con una escala" ~: duracionDelCaminoMasRapido [("BsAs", "Rosario", 5.0), ("Rosario", "Córdoba", 5.0), ("Córdoba", "BsAs", 8.0)] "BsAs" "Córdoba" ~?= 10.0,
     "duración del camino más rápido sin escala" ~: duracionDelCaminoMasRapido [("BsAs", "Rosario", 1.2)] "BsAs" "Rosario" ~?= 1.2,
     "duración del camino más rápido sin escala con vuelos en ambos sentidos " ~: duracionDelCaminoMasRapido [("Corrientes", "Rosario", 4.3),("Rosario","Corrientes",4.2)] "Corrientes" "Rosario" ~?= 4.3,
@@ -91,5 +93,57 @@ testsEjpuedoVolverAOrigen = test [
 
 
 -- Funciones extras
+
+-- margetFloat(): Float
+-- asegura: res es igual a 0.00001
+margenFloat = 0.00001
+
+-- expectAny (actual: a, expected: [a]): Test
+-- asegura: res es un Test Verdadero si y sólo si actual pertenece a la lista expected
 expectAny :: (Foldable t, Eq a, Show a, Show (t a)) => a -> t a -> Test
 expectAny actual expected = elem actual expected ~? ("expected any of: " ++ show expected ++ "\n but got: " ++ show actual)
+
+
+-- expectlistProximity (actual: [Float], expected: [Float]): Test
+-- asegura: res es un Test Verdadero si y sólo si:
+--                  |actual| = |expected|
+--                  para todo i entero tal que 0<=i<|actual|, |actual[i] - expected[i]| < margenFloat()
+
+expectlistProximity:: [Float] -> [Float] -> Test
+expectlistProximity actual expected = esParecidoLista actual expected ~? ("expected list: " ++ show expected ++ "\nbut got: " ++ show actual)
+
+esParecidoLista :: [Float] -> [Float] -> Bool
+esParecidoLista actual expected = (length actual) == (length expected) && (esParecidoUnaAUno actual expected)
+
+esParecidoUnaAUno :: [Float] -> [Float] -> Bool
+esParecidoUnaAUno [] [] = True
+esParecidoUnaAUno (x:xs) (y:ys) = (aproximado x y) && (esParecidoUnaAUno xs ys)
+
+aproximado :: Float -> Float -> Bool
+aproximado x y = abs (x - y) < margenFloat
+
+--expectAnyTuplaAprox (actual: CharxFloat, expected: [CharxFloat]): Test
+--  asegura: res un Test Verdadero si y sólo si:
+--                  para algun i entero tal que 0<=i<|expected|,
+--                         (fst expected[i]) == (fst actual) && |(snd expected[i]) - (snd actual)| < margenFloat()
+
+expectAnyTuplaAprox :: (Char, Float) -> [(Char, Float)] -> Test
+expectAnyTuplaAprox actual expected = elemAproxTupla actual expected ~? ("expected any of: " ++ show expected ++ "\nbut got: " ++ show actual)
+
+elemAproxTupla :: (Char, Float) -> [(Char, Float)] -> Bool
+elemAproxTupla _ [] = False
+elemAproxTupla (ac,af) ((bc,bf):bs) = sonAprox || (elemAproxTupla (ac,af) bs)
+    where sonAprox = (ac == bc) && (aproximado af bf)
+
+
+
+-- expectPermutacion (actual: [T], expected[T]) : Test
+-- asegura: res es un Test Verdadero si y sólo si:
+--            para todo elemento e de tipo T, #Apariciones(actual, e) = #Apariciones(expected, e)
+
+expectPermutacion :: (Ord a, Show a) => [a] -> [a] -> Test
+expectPermutacion actual expected = esPermutacion actual expected ~? ("expected list: " ++ show expected ++ "\nbut got: " ++ show actual)
+
+esPermutacion :: Ord a => [a] -> [a] -> Bool
+esPermutacion a b = (length a == length b) && (sort a == sort b)
+---}
